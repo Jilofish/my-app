00e56e9c1ba6c8a8af0bec621f0c6be6a478657b
Jilofish <115703434+Jilofish@users.noreply.github.com>
2024-10-15 22:29:57 +0800

    created session

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,62 +1,72 @@
 import { Providers } from "@/app/providers";
 import { PrismaClient } from "@prisma/client";
-import NextAuth from "next-auth"
+import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
 
 
 const prisma = new PrismaClient()
 
-
 export const authOptions = {
 
     session: {
         strategy: 'jwt'
     },
-
     providers: [
         CredentialsProvider({
             name: 'credentials',
             credentials: {
-                email: { label: "email", type:"email"},
+                email: { label: "email", type: "email" },
                 password: { label: "password", type: "password" }
             },
             async authorize(credentials, req) {
-            
-                if(!credentials.email || !credentials.password) {
+                // Add logic here to look up the user from the credentials supplied
+
+                if (!credentials.email || !credentials.password) {
                     return null;
                 }
-                
+
+                // user in DB
+
                 const user = await prisma.user.findUnique({
                     where: {
                         email: credentials.email
                     }
                 })
 
+
                 console.log('user found', user);
-
-
 
                 if (!user) {
                     return null;
-                } 
+                }
 
                 const isPasswordValid = await compare(credentials.password, user.password);
 
-                if(!isPasswordValid) {
+                if (!isPasswordValid) {
                     return null;
                 }
 
-
-                if (user) {
-                    return user
-                } else {
-                    return null
+                return {
+                    id: user.id,
+                    email: user.email,
+                    username: user.username,
+                    role: user.role,
+                    permissions: user?.permissions
                 }
+
 
             }
         })
-    ]
+    ],
+    pages: {
+        signIn: '/login'
+    },
+    debug: process.env.NODE_ENV === 'development',
+    jwt: {
+        secret: process.env.NEXTAUTH_JWT_SECRET
+    },
+    secret: process.env.NEXTAUTH_SECRET
 
 }
 
